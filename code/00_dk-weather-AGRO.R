a91b51e30432f7919e9d8af9eda7a0e4ea00f5e1
vanichols <virginia.nichols@gmail.com>
2024-04-19 15:29:06 +0200

    getting more weather

--- code/00_dk-weather-AGRO.R
+++ code/00_dk-weather-AGRO.R
@@ -9,11 +9,61 @@ library(scales)
 
 rm(list = ls())
 
-
+# get data from
+# https://agro-web11t.uni.au.dk/klimadb/
 
 # agro climate data -------------------------------------------------------
 
-d <- 
+d1 <- 
+  read_csv("data/raw/wea/1990-2013 AGRO-slagelse-US.csv") %>% 
+  rename(prec = prec08) %>% 
+  mutate(date = ymd(date),
+         doy = yday(date),
+         station = 613500) 
+
+tst <- 
+  d1 %>% 
+  mutate(minte2 = as.numeric(minte))
+
+tst2 <- 
+  tst %>% 
+  filter(is.na(minte2))
+
+#--are there more NAs in a given year?
+#--hmmm maybe this data isn't worth it
+#--could take averages of every other year and fill them in I guess
+tst2 %>% 
+  mutate(year = year(date)) %>% 
+  ggplot(aes(year)) + 
+  geom_histogram()
+
+d1a <- 
+  d1 %>% 
+  mutate_if(is.character, as.numeric) 
+
+d1b <- 
+  d1a %>%
+  group_by(doy) %>% 
+  mutate(minte_avg = mean(minte, na.rm = T),
+         maxte_avg = mean(maxte, na.rm = T),
+         temp_avg = mean(temp, na.rm = T),
+         prec_avg = mean(prec, na.rm = T)) %>% 
+  pivot_longer(minte:prec) %>% 
+  mutate(value = case_when(
+    (is.na(value) & name == "minte") ~ minte_avg,
+    (is.na(value) & name == "maxte") ~ maxte_avg,
+    (is.na(value) & name == "temp") ~ temp_avg,
+    (is.na(value) & name == "prec") ~ prec_avg,
+    TRUE ~ value
+  )) %>% 
+  pivot_wider(names_from = name, values_from = value) %>% 
+  select(station, date, doy, minte:prec)
+
+#--it worked...STOPPED
+d1b %>% 
+  filter(is.na(minte))
+
+d2 <- 
   read_csv("data/raw/wea/AGRO-slagelse-US.csv") %>% 
   mutate(date = dmy(date),
          doy = yday(date))
@@ -21,6 +71,9 @@ d <-
 summary(d)
 #--10 years, need more for long term but can write code
 
+d <- 
+  d1 %>% 
+  bind_rows(d2)
 
 # wea long term -----------------------------------------------------------
 
