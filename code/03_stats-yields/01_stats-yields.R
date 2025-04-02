a199f159688d4d85f7452c3492138e076fa63719
vanichols <virginia.nichols@gmail.com>
2025-04-02 07:47:52 +0200

    fixed yield stats

--- code/03_stats-yields/01_stats-yields.R
+++ code/03_stats-yields/01_stats-yields.R
@@ -48,6 +48,8 @@ d %>%
   geom_jitter()
 
 
+# 1. lmer -----------------------------------------------------------------
+
 #--I don't think we need subplot_id, as yield was not repeatedly measured on a subplot
 #--failed to converge
 #--I think this notation might be backwards? and you only need one?
@@ -63,21 +65,21 @@ m1 <- lmer(yield_dry_Mgha ~ till_id * cctrt_id * straw_id * crop +
 #--DenDF are 0...it is over-defined
 summary(m1)
 
-m2a <- lmer(yield_dry_Mgha ~ till_id * cctrt_id * straw_id * crop + 
+m1a <- lmer(yield_dry_Mgha ~ till_id * cctrt_id * straw_id * crop + 
              (1|block_id) +
              (1|cctrt_id:till_id:straw_id), #+ 
              #(1|till_id:straw_id) +
              #(1|straw_id), 
            data = d)
 
-m2b <- lmer(yield_dry_Mgha ~ till_id * cctrt_id * straw_id * crop + 
+m1b <- lmer(yield_dry_Mgha ~ till_id * cctrt_id * straw_id * crop + 
               (1|block_id),# +
               #(1|cctrt_id:till_id:straw_id), #+ 
             #(1|till_id:straw_id) +
             #(1|straw_id), 
             data = d)
 
-m2c <- lmer(yield_dry_Mgha ~ cctrt_id * till_id * straw_id * cctrt_id * crop + 
+m1c <- lmer(yield_dry_Mgha ~ cctrt_id * till_id * straw_id * cctrt_id * crop + 
               (1|block_id) +
               #(1|block_id:straw_id) +
               (1|block_id:straw_id:till_id),
@@ -85,6 +87,9 @@ m2c <- lmer(yield_dry_Mgha ~ cctrt_id * till_id * straw_id * cctrt_id * crop +
             #(1|straw_id), 
             data = d)
 
+
+
+# 2. nlme -----------------------------------------------------------------
 
 library(nlme)
 # model <- lme(Y ~ W_Treat * S_Treat * SS_Treat, 
@@ -95,11 +100,13 @@ m2d <- lme(yield_dry_Mgha ~ cctrt_id * till_id * straw_id * cctrt_id * crop,
            random = ~1 | block_id/straw_id/till_id,
             data = d)
 
+#--variance differs by block
 m2e <- lme(yield_dry_Mgha ~ cctrt_id * till_id * straw_id * cctrt_id * crop,
            random = ~1 | block_id/straw_id/till_id,
            data = d,
            weights = varIdent(form = ~1 | block_id))
 
+#--variance differs by cctrt
 m2f <- lme(yield_dry_Mgha ~ cctrt_id * till_id * straw_id * cctrt_id * crop,
            random = ~1 | block_id/straw_id/till_id,
            data = d,
@@ -111,7 +118,17 @@ anova(m2d, m2f)
 anova(m2e, m2f)
 AIC(m2d, m2e, m2f)
 
-#--look at variances btwn blocks, this seems like the best model
+#m2e seems to be the best, but not statistically better than m2f
+# accounting for some type of hetergeneous variance seems good
+#--this gives a sig 3-way interaction
+anova(m2e)
+#--this gives a cctrt x crop interaction
+anova(m2f)
+
+#--cctrt and crop are always sig, the others change depend on model
+
+#--heterogeneous variance should be hypothesis driven. Why would it vary?
+#--look at variances btwn blocks
 ggplot(data = data.frame(resid = residuals(m2d), 
                          block_id = d$block_id)) + 
   geom_boxplot(aes(x = block_id, y = resid)) + 
@@ -123,147 +140,72 @@ ggplot(data = data.frame(resid = residuals(m2d),
   geom_boxplot(aes(x = cctrt_id, y = resid)) + 
   theme_minimal()
 
-#--use glmmTMB
-mod_het_TMB <- glmmTMB(Y ~ W_Treat * S_Treat * SS_Treat + 
-                         (1 | Block) + (1 | Block:W_Treat) + (1 | Block:W_Treat:S_Treat),
-                       data = mydata,
-                       dispformula = ~ W_Treat) 
+#--I have no physical reason to assume variance would differ,
+#--therefore don't include it
 
-anova(m2a)
-anova(m2b)
-anova(m2c)
-anova(m2d)
-#--model c seems to make more sense
-AIC(m2c, m2b)
-AIC(m2c, m2d) #--these should be the same model
+#--use glmmTMB notation
+# mod_het_TMB <- glmmTMB(Y ~ W_Treat * S_Treat * SS_Treat + 
+#                          (1 | Block) + (1 | Block:W_Treat) + (1 | Block:W_Treat:S_Treat),
+#                        data = mydata,
+#                        dispformula = ~ W_Treat) 
+
+anova(m1a) #--p values of 1?
+anova(m1b) #--more sensical
+anova(m1c) #--only crop and cctrt
+anova(m2d) #--only crop and cctrt
+AIC(m1c, m2d) #--these should be the same model
+
+#--model c/d seems to make more sense
+AIC(m1c, m1b)
+AIC(m1c, m2d)
 #--model c or d has lower AIC
 
 #--Maarit says don't worry about what the DHARMa is flagging
 library(DHARMa)
-plot(simulateResiduals(m2c))
-plot(m2c)
+plot(simulateResiduals(m1c))
+plot(m1c)
 #--this looks good, the pattern is fine
-qqnorm(resid(m2c))
-qqline(resid(m2c))
+qqnorm(resid(m1c))
+qqline(resid(m1c))
 
+mwin <- m2c
+anova(mwin)
+tidy(anova(mwin))
 
+#--marginal means
+em_crop <- emmeans(mwin, ~crop)
+em_cc <- emmeans(mwin, ~cctrt_id)
 
-m2 <- m2e
-anova(m2)
-tidy(anova(m2))
-
-em_crop <- emmeans(m2e, ~crop)
-
-em_cc <- emmeans(m2e, ~cctrt_id)
-
-tidy(pairs(emmeans(m2e, ~ cctrt_id))) %>% 
+#--radM is higher than all of them, all others equal
+tidy(pairs(emmeans(mwin, ~ cctrt_id))) %>% 
   filter(adj.p.value < .05)
 
 em_cc_means <- 
   tidy(em_cc) %>% 
   arrange(-estimate) %>%  
-  mutate(letter_sig = c("a", "ab", "b", "b", "b"),
+  mutate(letter_sig = c("a", "b", "b", "b", "b"),
          resp_var = "crop_yield") 
 
 em_cc_means %>% 
   write_csv("data/em_crop-yields-by-cc.csv")
 
-#-this is goofy now that I'm using nlme
-anova(m2e)
-r <- tidy(anova(m2e)) 
+r <- 
+  as_tibble(anova(mwin)) %>% 
+  mutate(term = row.names(anova(mwin))) %>% 
+  select(term, everything())
 
 r %>% 
   write_csv("data/stats_yields.csv")
 
 
-# 
-# r %>% 
-#   filter(grepl("crop", term)) %>% 
-#   filter(term != "crop")
-# 
-# #--effect of crop, but no interactions
-# #--effect of cover crop, no interactions
-# #--effect of tillage, depending on straw
-# 
+
+#--effect of crop, but no interactions
+#--effect of cover crop, no interactions
+
 # #--should present it as cover crop as that is the focus
-# 
+
 # # cover crops -------------------------------------------------------------
-# em_cc <- emmeans(m2, specs = pairwise ~ cctrt_id)
-# tidy(em_cc$contrasts) %>%
-#   filter(adj.p.value < 0.05)
-# #--nocc is lower than rad_M
-# #--rad_L is lower than rad_M
-# #--mix_M is lower than rad_M
-# #--mixE is lower than radM
-# 
-# em_cc_means <- 
-#   tidy(em_cc$emmeans) %>% 
-#   arrange(-estimate) %>%  
-#   mutate(letter_sig = c("a", "b", "b", "b", "b"),
-#          resp_var = "crop_yield") 
-# 
-# em_cc_means %>% 
-#   write_csv("data/em_crop-yields-by-cc.csv")
-# 
-# em_cc_means %>% 
-#   ggplot(aes(cctrt_id, estimate)) +
-#   geom_errorbar(aes(x = cctrt_id, ymin = estimate - std.error, ymax = estimate + std.error),
-#                 width = 0.2) +
-#   geom_point() +
-#   geom_jitter(data = d, aes(cctrt_id, estimate, color = crop), width = 0.1) +
-#   geom_text(aes(x = cctrt_id, y = estimate + 0.3, label = letter_sig)) +
-#   scale_y_continuous(limits = c(0, 4.5))
-# 
-# 
 # em_cc
 # radM <- c(0, 0, 0, 0, 1)
 # mixes <- c(0.5, 0.5, 0, 0, 0)
 # contrast(em_cc, method = list("radM - mixes" = radM - mixes))
-# 
-# 
-# # tillage by straw by crop--------------------------------------------------------
-# 
-em_ts <- emmeans(m2, specs = pairwise ~ till_id:straw_id:crop)
-
-res_tsc <- 
-  em_ts$contrasts %>% 
-  tidy(.) %>% 
-  filter(adj.p.value < 0.05)
-
-res_tsc2 <- 
-  res_tsc %>% 
-  separate(contrast, sep = "-", into = c("C1", "C2")) %>% 
-  mutate(C1 = str_squish(C1),
-         C2 = str_squish(C2)) %>% 
-  separate(C1, sep = " ", into = c("t1", "s1", "c1")) %>% 
-  separate(C2, sep = " ", into = c("t2", "s2", "c2")) 
-
-
-d %>% 
-  filter(till_id == "notill", 
-         straw_id == "removed") %>% 
-  ggplot(aes(crop, yield_dry_Mgha)) +
-  geom_point()
-
-#--when removing straw, no-till yields less than the other tillages
-#--when retaining straw, no difference in yields
-# 
-# #--compare no-till to other two tillages for each 
-# notillnostraw <- c(0, 0, 1, 0, 0, 0)
-# othersnostraw <- c(0.5, 0.5, 0, 0, 0, 0)
-# 
-# 
-# contrast(em_ts, method = list("notill - all" = notill - othersnostraw) )
-# 
-# # emmeans for all (except crop)---------------------------------------------------------
-# 
-# em_all <- tidy(emmeans(m2, specs = pairwise ~ till_id|straw_id|cctrt_id)$emmeans) 
-# 
-# em_all %>% 
-#   ggplot(aes(straw_id, estimate)) +
-#   geom_errorbar(aes(x = straw_id, ymin = estimate - std.error, ymax = estimate + std.error, 
-#                     color = cctrt_id),
-#                 width = 0.2) +
-#   geom_point(aes(color = cctrt_id)) +
-#   facet_grid(.~till_id) +
-#   scale_y_continuous(limits = c(0, 4.7))
