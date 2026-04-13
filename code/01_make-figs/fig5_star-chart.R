b08591565f6a8723499318ba8c0539b00a5b3dba
vanichols <virginia.nichols@gmail.com>
2026-04-13 11:54:15 +0200

    Fix cutoff Saf and res labels

--- code/01_make-figs/fig5_star-chart.R
+++ code/01_make-figs/fig5_star-chart.R
@@ -1,6 +1,10 @@
 #--make a circular barplot
 #//look at 6 perfomance categories together
 
+#--need to run with updated emmeans values for fig
+#--need to increase distance between left axis label and figure text (Saf is covered)
+#--change to res+ and res- (+res and -res right now)
+
 rm(list = ls())
 
 library(tidyverse)
@@ -38,7 +42,7 @@ d2 <-
 
 d3 <- 
   MakeNiceLabels(d2) %>%  
-  mutate(straw_nice = ifelse(straw_id == "retained", "+res", "-res"),
+  mutate(straw_nice = ifelse(straw_id == "retained", "(+)Residue", "(-)Residue"),
          till_nice = case_when(
            till_nice == "Inv"~"Inversion",
            till_nice == "Surf"~"Surface",
@@ -71,7 +75,15 @@ d5 %>%
   scale_y_continuous(limits = c(0, 1),
                      breaks = c(0, 0.25, 0.5, 0.75, 1)) +
   facet_nested(cctrt_nice~till_nice + straw_nice,
-               switch = "y") +
+               switch = "y", 
+               strip = strip_nested(
+                 text_x = list(
+                   element_text(size = rel(1.3), "bold"),  # till_nice (outer)
+                   element_text(size = rel(1.3), "italic")    # straw_nice (inner)
+                 ),
+                 text_y = element_text(size = rel(1.3))  # cctrt_nice (rows)
+               )
+                 ) +
   coord_polar(clip = "off") +
   labs(x = NULL,
        y = NULL,
@@ -83,13 +95,12 @@ d5 %>%
         panel.border = element_blank(),
         legend.position = "bottom",
         legend.title.position = "top",
-        strip.background.x = element_rect(fill = "white", 
+        strip.background.x = element_rect(fill = "transparent", 
                                           color = "white"),
-        strip.background.y = element_rect(fill = "white", 
+        strip.background.y = element_rect(fill = "transparent", 
                                           color = "white"),
-        strip.text = element_text(size = rel(1.3))) 
+        plot.margin=unit(c(1,1,1.5,1.2),"cm")) 
 
 
 ggsave("figs/fig5_total-value.png", height = 9, width = 9)
-
 
